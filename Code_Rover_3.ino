b983b990a674c0f27b41fc7f3e77fd13791f5d79
AlexisCode101 <33733267+AlexisCode101@users.noreply.github.com>
2017-12-01 15:07:00 -0500

    Update Code_Rover_3.ino

--- Code_Rover_3.ino
+++ Code_Rover_3.ino
@@ -11,21 +11,22 @@ stop de [0 à 25[
 #define    MeasureValue        0x04          // Value to initiate ranging.
 #define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
 
-#define sensor0 A1 //gauche 
+#define sensor0 A3 //gauche 
 #define sensor1 A2 //droite
-#define sensor2 A3 //avant
+#define sensor2 A4 //avant
 #include <Servo.h>
 
 Servo Lidar;
 Servo MoteurAvance;
 Servo MoteurTourne;
 
-int pos = 0;         // Position of the servo (degress, [0, 180])
-int distance = 0;    // Distance measured
+double pos = 0;         // Position of the servo (degress, [0, 180])
+double distance = 0;    // Distance measured
 int distanceIR0=0;
 int distanceIR1=0;
 int distanceIR2=0;
 int distanceParcouru=0;
+
 
 const int MoteurAvancePin = 8;  // avance
 const int MoteurTournePin = 9; // tourne
@@ -78,16 +79,16 @@ int lidarGetRange(void)
 int iRGetRange(void)
 {
   float volts0=analogRead(sensor0)*0.0048828125;
-  int distanceIR0=13*pow(volts0,-1)*2.54;
+  distanceIR0=13*pow(volts0,-1)*2.54;
   float volts1=analogRead(sensor1)*0.0048828125;
-  int distanceIR1=13*pow(volts1,-1)*2.54;
+  distanceIR1=13*pow(volts1,-1)*2.54;
   float volts2=analogRead(sensor2)*0.0048828125;
-  int distanceIR2=13*pow(volts2,-1)*2.54;
-  
+  distanceIR2=13*pow(volts2,-1)*2.54;
+
   //Serial.println(distanceIR0);
-  //Serial.print("gauche");
-  Serial.println(distanceIR1);
-  Serial.print("droite \n");
+  //Serial.print("gauche \n");
+  //Serial.println(distanceIR1);
+  //Serial.print("droite \n");
   //Serial.println(distanceIR2);
   //Serial.print("arriere");
 }
@@ -102,7 +103,7 @@ void serialPrintRange(int pos, int distance)
 
 void LIDAR(void)
 {
- 
+   
   for(pos = 40; pos <= 140; pos += 5)
   {
     iRGetRange();
@@ -111,8 +112,8 @@ void LIDAR(void)
     distance = lidarGetRange();
     serialPrintRange(pos, distance);
   }
-  
-  for(pos = 140; pos>=40; pos-=5)
+
+  for(pos = 140; pos>=40; pos -= 5)
   {
     iRGetRange();
     avance();
@@ -122,65 +123,66 @@ void LIDAR(void)
   }
 }
 void avance(void)
-{
-   if(distanceIR1<20) 
+{ 
+   if(distanceIR1<20 && distanceIR1>1 ) 
    {
-   //Serial.println(distanceIR1);
     MoteurAvance.write(90);
-    delay(100); 
-    MoteurAvance.write(50);
+    delay(50); 
+    MoteurAvance.write(40);
     delay(500);
     MoteurAvance.write(90);
-    delay(100);
+    delay(50);
    }
    else
    {
-    if(distance>50)
+    if(distance > (-0.0112*(pos)*(pos) + 2.0162*(pos) - 39.25))
     {
      //MoteurAvance.write(148.73); vitesse theorique
      MoteurAvance.write(110); //vitesse test
     }   
-    else if(pos>=60 && pos<=120)
+    else //if(pos>=70 && pos<=110)
     {
      MoteurAvance.write(90);
-     delay(100); 
+     delay(1); 
       if(pos<=90)
       {
        MoteurTourne.write(140);
        delay(8*pos-220);
        MoteurTourne.write(90);
-       delay(100);
+       delay(1);
       } 
       else
       {
        MoteurTourne.write(40);
        delay(-8*pos+1220);
        MoteurTourne.write(90);
-       delay(100);
+       delay(1);
        } 
      }
+     /*
      else
      {
-      if(distance<20)
+      if(distance<30)
       {
        MoteurAvance.write(90);
-       delay(100); 
+       delay(10); 
        if(pos<=90)
        {
         MoteurTourne.write(140);
         delay(8*pos-220);
         MoteurTourne.write(90);
-        delay(100);
+        delay(10);
        } 
        else
        {
         MoteurTourne.write(40);
         delay(-8*pos+1220);
         MoteurTourne.write(90);
-        delay(100);
+        delay(10);
        } 
       }
      }
+     */
     }
 }
 
@@ -188,5 +190,3 @@ void loop()
 {
   LIDAR();
 }
-
-
