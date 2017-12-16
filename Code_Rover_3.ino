09e3f18141d44ea006cfdf5b0ee1da48af79c6c0
AlexisCode101 <33733267+AlexisCode101@users.noreply.github.com>
2017-12-16 12:32:52 -0500

    Update Code_Rover_3.ino

--- Code_Rover_3.ino
+++ Code_Rover_3.ino
@@ -1,192 +1,107 @@
-/***********************************************************************************************************
-Reculer de [25 à 90]
-Avancer de [94 à 160]
-stop de [0 à 25[
-/**********************************************************************************************************/
+  #include <ESP8266WiFi.h>
 
-#include <Wire.h>
+  #define     LED0      2           // LED du module WIFI
+  #define     LED1      D0          // Connectivité avec le client #1
+  #define     BOUTON    D1          // bouton de réinitialisation de la connectivité
 
-#define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
-#define    RegisterMeasure     0x00          // Register to write to initiate ranging.
-#define    MeasureValue        0x04          // Value to initiate ranging.
-#define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
+  char*       ROVERssid;           // nom du serveur wifi
+  char*       ROVERpassword;        // mot de passe du serveur
 
-#define sensor0 A3 //gauche 
-#define sensor1 A2 //droite
-#define sensor2 A4 //avant
-#include <Servo.h>
+  #define     MAXSC     1           // nombre maximal de client
+  WiFiServer  ROVERServer(9001);    // le serveur et son numéro de port
+  WiFiClient  ROVERClient[MAXSC];   // le serveur des clients
 
-Servo Lidar;
-Servo MoteurAvance;
-Servo MoteurTourne;
-
-double pos = 0;         // Position of the servo (degress, [0, 180])
-double distance = 0;    // Distance measured
-int distanceIR0=0;
-int distanceIR1=0;
-int distanceIR2=0;
-int distanceParcouru=0;
-
-
-const int MoteurAvancePin = 8;  // avance
-const int MoteurTournePin = 9; // tourne
-
-
-void setup()
-{
-  // Serial output
-  Serial.begin(9600);
-  Serial.println("< START >");
-  
-  // Servo control
-  Lidar.attach(7); //Servo du LIDAR
-  MoteurAvance.attach(MoteurAvancePin,1000,2000); //Servo avancer
-  MoteurTourne.attach(MoteurTournePin,1000,2000); //Servo tourner
-  // LIDAR control
-  Wire.begin(); // join i2c bus
-}
-
-// Get a measurement from the LIDAR Lite
-int lidarGetRange(void)
-{
-  int val = -1;
-  
-  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)  
-  Wire.write((int)MeasureValue); // sets register pointer to  (0x00)  
-  Wire.endTransmission(); // stop transmitting
-
-  delay(20); // Wait 20ms for transmit
-
-  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
-  Wire.endTransmission(); // stop transmitting
-
-  delay(20); // Wait 20ms for transmit
-  
-  Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
-
-  if(2 <= Wire.available()) // if two bytes were received
+  void setup()
   {
-    val = Wire.read(); // receive high byte (overwrites previous reading)
-    val = val << 8; // shift high byte to be high 8 bits
-    val |= Wire.read(); // receive low byte as lower 8 bits
+    // Setting The Serial Port
+    Serial.begin(115200);           // Communication avec l'ordinateur
+    
+    // Setting The Mode Of Pins
+    pinMode(LED0, OUTPUT);          // LED sur le board WIFI
+    pinMode(LED1, OUTPUT);          // Indicateur de la connectivité du client
+    pinMode(BOUTON, INPUT_PULLUP);  // Initialise la connectivité
+    
+    // Affiche la progression de la configuration I/O
+    Serial.println();
+    Serial.println("I/O Pins Modes Set .... Done");
+
+    // Configure le point d'accès WIFI
+    SetWifi("Alexis_ROVER_3", "");
   }
   
-  return val;
-}
-
-int iRGetRange(void)
-{
-  float volts0=analogRead(sensor0)*0.0048828125;
-  distanceIR0=13*pow(volts0,-1)*2.54;
-  float volts1=analogRead(sensor1)*0.0048828125;
-  distanceIR1=13*pow(volts1,-1)*2.54;
-  float volts2=analogRead(sensor2)*0.0048828125;
-  distanceIR2=13*pow(volts2,-1)*2.54;
-
-  //Serial.println(distanceIR0);
-  //Serial.print("gauche \n");
-  //Serial.println(distanceIR1);
-  //Serial.print("droite \n");
-  //Serial.println(distanceIR2);
-  //Serial.print("arriere");
-}
-
-void serialPrintRange(int pos, int distance)
-{
-    //Serial.print("Position (deg): ");
-    //Serial.print(pos);
-    //Serial.print("\t\tDistance (cm): ");
-    //Serial.println(distance);
-}
-
-void LIDAR(void)
-{
-   
-  for(pos = 40; pos <= 140; pos += 5)
+  void loop()
   {
-    iRGetRange();
-    avance();
-    Lidar.write(pos);
-    distance = lidarGetRange();
-    serialPrintRange(pos, distance);
+    
+    IsClient();
+    
   }
 
-  for(pos = 140; pos>=40; pos -= 5)
+  
+  void SetWifi(char* Name, char* Password)
   {
-    iRGetRange();
-    avance();
-    Lidar.write(pos);
-    distance = lidarGetRange();
-    serialPrintRange(pos, distance);
+    // Arrête tous les WIFI précédents
+    WiFi.disconnect();
+
+    // Configuration du mode WIFI
+    WiFi.mode(WIFI_AP_STA);
+    Serial.println("Mode du WIFI : Point d'accès");
+    
+    // Configuration du point d'accès
+    ROVERssid      = Name;
+    ROVERpassword  = Password;
+    
+    // Démarrage du point d'accès
+    WiFi.softAP(ROVERssid, ROVERpassword);
+    Serial.println("WIFI < " + String(ROVERssid) + " > ... Demarre");
+    
+    delay(1000);
+    
+    // Obtention de l'IP du serveur
+    IPAddress IP = WiFi.softAPIP();
+    
+    // Impression de l'adresse IP du serveur
+    Serial.print("IP du point d'accès : ");
+    Serial.println(IP);
+
+    // Démarrage du Serveur
+    ROVERServer.begin();
+    Serial.println("Serveur demarré");
   }
-}
-void avance(void)
-{ 
-   if(distanceIR1<20 && distanceIR1>1 ) 
-   {
-    MoteurAvance.write(90);
-    delay(50); 
-    MoteurAvance.write(40);
-    delay(500);
-    MoteurAvance.write(90);
-    delay(50);
-   }
-   else
-   {
-    if(distance > (-0.0112*(pos)*(pos) + 2.0162*(pos) - 39.25))
+  
+  void IsClient()
+  {
+    if (ROVERServer.hasClient())
     {
-     //MoteurAvance.write(148.73); vitesse theorique
-     MoteurAvance.write(110); //vitesse test
-    }   
-    else //if(pos>=70 && pos<=110)
-    {
-     MoteurAvance.write(90);
-     delay(1); 
-      if(pos<=90)
+      for(int i = 0; i < MAXSC; i++)
       {
-       MoteurTourne.write(140);
-       delay(8*pos-220);
-       MoteurTourne.write(90);
-       delay(1);
-      } 
-      else
-      {
-       MoteurTourne.write(40);
-       delay(-8*pos+1220);
-       MoteurTourne.write(90);
-       delay(1);
-       } 
-     }
-     /*
-     else
-     {
-      if(distance<30)
-      {
-       MoteurAvance.write(90);
-       delay(10); 
-       if(pos<=90)
-       {
-        MoteurTourne.write(140);
-        delay(8*pos-220);
-        MoteurTourne.write(90);
-        delay(10);
-       } 
-       else
-       {
-        MoteurTourne.write(40);
-        delay(-8*pos+1220);
-        MoteurTourne.write(90);
-        delay(10);
-       } 
+        if (!ROVERClient[i] || !ROVERClient[i].connected())
+        {
+          if(ROVERClient[i]) ROVERClient[i].stop();
+          ROVERClient[i] = ROVERServer.available();
+          Serial.print("New Client : "); Serial.print(String(i+1) + " - ");
+          continue;
+        }
       }
-     }
-     */
+      digitalWrite(LED1, HIGH);
+      WiFiClient ROVERClient = ROVERServer.available();
+      ROVERClient.stop();
     }
-}
-
-void loop()
-{
-  LIDAR();
-}
+    
+    for(int i = 0; i < MAXSC; i++)
+    {
+      if (ROVERClient[i] && ROVERClient[i].connected())
+      {
+        if(ROVERClient[i].available())
+        {
+          // S'il y a des données, on lit
+          while(ROVERClient[i].available()) 
+          {
+            // Lit du client
+            Serial.println(ROVERClient[i].readStringUntil('\n'));
+            // Répond au client
+            ROVERClient[i].println("<OK>");
+          }
+        }
+      }
+    }
+  }
