f06abd9e8b9c9ad9383e7c0791fcb7969c5c7028
AlexisCode101 <33733267+AlexisCode101@users.noreply.github.com>
2017-12-16 12:28:19 -0500

    Update Client_JOYA.ino

--- Client_JOYA.ino
+++ Client_JOYA.ino
@@ -1,133 +1,72 @@
 //------------------------------------------------------------------------------------
   #include <Wire.h>
   #include <ESP8266WiFi.h>
-//------------------------------------------------------------------------------------
-// Defining I/O Pins
-//------------------------------------------------------------------------------------
-  #define       LED0      2         // WIFI Module LED
-  #define       LED1      D0        // Connectivity With Client #1
-  #define       BOUTON    D1        // Connectivity ReInitiate Button
-  #define       TWI_FREQ  400000L   // I2C Frequency Setting To 400KHZ
-//------------------------------------------------------------------------------------
-// BUTTON Variables
-//------------------------------------------------------------------------------------
-  int           ButtonState;
-  int           LastButtonState   = LOW;
-  int           LastDebounceTime  = 0;
-  int           DebounceDelay     = 50;
-  const String  ButtonColor       = "Alexis";
-//------------------------------------------------------------------------------------
-// LED Delay Variables
-//------------------------------------------------------------------------------------
+
+  #define       LED0      2         
+  #define       LED1      D0        
+  #define       RX        3
+  #define       TWI_FREQ  400000L   // Fréquence de l'I2C à 400KHZ
+
   int           LEDState          = LOW;
   unsigned long CurrMillis        = 0;
   unsigned long PrevMillis        = 0;
   unsigned long Interval          = 1000;
-//------------------------------------------------------------------------------------
-// Authentication Variables
-//------------------------------------------------------------------------------------
+
   char*         ROVERssid;
   char*         ROVERpassword;
   IPAddress     ROVERServer(192,168,4,1);
   WiFiClient    ROVERClient;
-//====================================================================================
+
+char str[4];
 
   void setup() 
   {
-    /* ----------------------------------------------------------------------
-     * Setting The I2C Pins SDA, SCL
-     * Because We Didnt Specify Any Pins The Defult
-     * SDA = D4 <GPIO2>, SCL = D5 <GPIO14> For ESP8266 Dev Kit Node MCU v3
-     --------------------------------------------------------------------- */
-    Wire.begin();                   // Begginning The I2C
+    Wire.begin();                   
     
-    // Setting Up The I2C Of The MPU9250 ------------------------------------
-    Wire.setClock(TWI_FREQ);        // Setting The Frequency MPU9250 Require
-        
-    // Setting The Serial Port ----------------------------------------------
-    Serial.begin(115200);           // Computer Communication
+    Wire.setClock(TWI_FREQ);        // Règle la fréquence requise pour le MPU9250
+           
+    Serial.begin(115200);           
     
-    // Setting The Mode Of Pins ---------------------------------------------
-    pinMode(LED0, OUTPUT);          // WIFI OnBoard LED Light
-    pinMode(LED1, OUTPUT);          // Indicator For Client #1 Connectivity
-    pinMode(BOUTON, INPUT_PULLUP);  // Initiate Connectivity
-    digitalWrite(LED0, !LOW);       // Turn WiFi LED Off
-    
-    // Print Message Of I/O Setting Progress --------------------------------
-    Serial.println("\nI/O Pins Modes Set .... Done");
+    pinMode(LED0, OUTPUT);         
+    pinMode(LED1, OUTPUT);          
+    pinMode(RX,OUTPUT);
+    digitalWrite(LED0, !LOW);       
 
-    // Starting To Connect --------------------------------------------------
-    WiFi.mode(WIFI_STA);            // To Avoid Broadcasting An SSID
-    WiFi.begin("Alexis_ROVER_3","");          // The SSID That We Want To Connect To
+    WiFi.mode(WIFI_STA);            
+    WiFi.begin("Alexis_ROVER_3","");          //SSID cible
 
-    // Printing Message For User That Connetion Is On Process ---------------
     Serial.println("!--- Connecting To " + WiFi.SSID() + " ---!");
 
-    // WiFi Connectivity ----------------------------------------------------
-    CheckConnectivity();            // Checking For Connection
 
-    // Stop Blinking To Indicate Connected ----------------------------------
+    CheckConnectivity();        
+
+    // Arret du clignotement pour dire quil est connecté 
     digitalWrite(LED0, !HIGH);
-    Serial.println("!-- Client Device Connected --!");
+    Serial.println("!-- Peripheriqe client est connecte --!");
 
-    // Printing IP Address --------------------------------------------------
-    Serial.println("Connected To      : " + String(WiFi.SSID()));
-    Serial.println("Signal Strenght   : " + String(WiFi.RSSI()) + " dBm");
-    Serial.print  ("Server IP Address : ");
+    Serial.println("Connecte a                  : " + String(WiFi.SSID()));
+    Serial.println("Force du signal             : " + String(WiFi.RSSI()) + " dBm");
+    Serial.print  ("Adresse IP du serveur       : ");
     Serial.println(ROVERServer);
-    Serial.print  ("Device IP Address : ");
+    Serial.print  ("Addresse IP du peripherique : ");
     Serial.println(WiFi.localIP());
     
-    // Conecting The Device As A Client -------------------------------------
+    // Conection en tant que client 
     ROVERRequest();
   }
-
-//====================================================================================
   
   void loop()
   {
 
-    ReadButton();
+    sendSerial();
   
   }
 
-//====================================================================================
-
-  void ReadButton()
+  void sendSerial()
   {
-  /*  // Reading The Button
-    int reading = digitalRead(BUTTON);
-
-    // If It Doest Match The Previous State
-    if(reading != LastButtonState)
-    {
-      LastDebounceTime = millis();
-    }
-
-    // To Iliminate Debounce
-    if((millis() - LastDebounceTime) > DebounceDelay)
-    {
-      if(reading != ButtonState)
-      {
-        ButtonState = reading;
-        
-        if(ButtonState == LOW)
-        {
-          LEDState    = !digitalRead(LED1);
-          digitalWrite(LED1, LEDState);
-          Serial.println("<" + ButtonColor + "-SCORED>");*/
-         delay(1000);
-          ROVERClient.println("<" + ButtonColor + " is the greatest");
-          ROVERClient.flush();
-        /*}
-      }
-    }
-
-    // Last Button State Concidered
-    LastButtonState = reading;*/
+         ROVERClient.println("Test");
+         ROVERClient.flush();
   }
-
-//====================================================================================
 
   void CheckConnectivity()
   {
@@ -145,19 +84,12 @@
     }
   }
 
-//====================================================================================
-
   void ROVERRequest()
   {
-    // First Make Sure You Got Disconnected
     ROVERClient.stop();
-
-    // If Sucessfully Connected Send Connection Message
     if(ROVERClient.connect(ROVERServer, 9001))
     {
-      Serial.println    ("<" + ButtonColor + "-CONNECTED M8>");
-      ROVERClient.println ("<" + ButtonColor + "-CONNECTED M8>");
+      Serial.println    ("<ROVER 3-CONNECTE>");
+      ROVERClient.println ("<ROVER 3-CONNECTE>");
     }
   }
-
-//====================================================================================
